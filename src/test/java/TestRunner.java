5cd18495f3270cbc61ac75cc01939bda9270f3ed
Aliaksandr Zhukovich <Aliaksandr_Zhukovich@epam.com>
2020-01-10 13:31:20 +0300

    Cucumber with Extent report.

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,12 +1,15 @@
 
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -17,7 +20,9 @@ import org.testng.annotations.Test;
                 "pretty",
                 "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/cucumber-reports/rerun.txt"
+                "rerun:target/cucumber-reports/rerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+
         }
         )
 public class TestRunner {
